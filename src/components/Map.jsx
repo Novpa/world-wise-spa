286b04349a5e079755744e401fbcf89ebc84be90
Novpa <aa.novparadiputra@binus.ac.id>
2026-01-07 14:43:37 +0800

    Ref: Adding documentation to the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -28,6 +28,8 @@ function Map() {
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
 
+  console.log(mapLat, mapLng);
+
   //Change the searchParams, we need to pass in brand new object to the setSearchParams and then set the new value there (with the complete properties)
   //  <button onClick={() => setSearchParams({ lat: 25, lng: 90 })}>
   //       Change the SearchParams
@@ -37,6 +39,7 @@ function Map() {
     if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
   }, [mapLat, mapLng]);
 
+  // Use to set the mapPosition to the current user's position whenever the user click Button "Use your position"
   useEffect(() => {
     if (geolocationPosition)
       setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
@@ -54,8 +57,7 @@ function Map() {
         // center={[mapLat, mapLng]}
         zoom={6}
         scrollWheelZoom={true}
-        className={styles.map}
-      >
+        className={styles.map}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
@@ -63,8 +65,7 @@ function Map() {
         {cities.map((city) => (
           <Marker
             key={city.id}
-            position={[city.position.lat, city.position.lng]}
-          >
+            position={[city.position.lat, city.position.lng]}>
             <Popup>
               <span>{city.emoji}</span> <span>{city.cityName}</span>
             </Popup>
@@ -77,6 +78,7 @@ function Map() {
   );
 }
 
+// This will center the map everytime we click to different location in the map
 function ChangeCenter({ position }) {
   const map = useMap();
   map.setView(position);
@@ -84,6 +86,7 @@ function ChangeCenter({ position }) {
   return null;
 }
 
+// This will return the data that is clicked by the user
 function DetectClick() {
   const navigate = useNavigate();
 
@@ -91,6 +94,9 @@ function DetectClick() {
     click: (e) => {
       console.log(e);
       navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+      // This will navigate/change the link with global data {lat & lng} -> change the {mapLat & mapLng} -> triggers the useEffect -> change mapPosition thru setMapPosition inside of th useEffect -> triggers the <ChangeCenter /> to center the map to the clicked position
+
+      //If we are using only navigate(`form`), this only pop up the Form without centering the map;
     },
   });
 }
